b6a5c3d39ccabd79b4718990db84d2b4b0ded786
Nirmansutraa <nirmansutraa@gmail.com>
2026-04-02 22:24:16 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
+/* NIRMANSUTRA MASTER CONFIG - MARCH 2026 */
+const firebaseConfig = {
+    apiKey: "AIzaSyDi2TQAvaqnz0D3eK6KZLYYhxsHUBG10A8",
+    authDomain: "infradepo.firebaseapp.com",
+    projectId: "infradepo",
+    databaseURL: "https://infradepo-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
 
+if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig); }
+const auth = firebase.auth();
+const rtdb = firebase.database();
