d0a30b87720588e5c6c0e4fe8116078acecc8b6f
Nirmansutraa <nirmansutraa@gmail.com>
2026-04-02 22:25:06 +0530

    Update ui-utils.js

--- js/ui-utils.js
+++ js/ui-utils.js
@@ -1,1 +1,20 @@
-
+/* NIRMANSUTRA COMMON UI UTILS */
+window.NS_UI = {
+    toggleEye(id) {
+        const el = document.getElementById(id);
+        if (el) el.type = el.type === 'password' ? 'text' : 'password';
+    },
+    
+    notify(msg) { alert("NirmanSutra: " + msg); },
+    
+    // Multi-Field Resolver (From your Super Admin code)
+    resolveField(dataObj, keyVariations) {
+        if (!dataObj) return "N/A";
+        for (let targetKey of keyVariations) {
+            if (dataObj[targetKey] && dataObj[targetKey] !== "" && dataObj[targetKey] !== "N/A") {
+                return dataObj[targetKey];
+            }
+        }
+        return "N/A";
+    }
+};
